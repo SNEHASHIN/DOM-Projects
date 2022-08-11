18d564813b398eddc3e5bf50acb20cd6466b3fea
sneha shinde <Sneha06032000@gmail.com>
2022-08-11 11:50:27 +0530

    color flipper

---
+++ Color Flipper/index.js
@@ -0,0 +1,14 @@
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+const colors = ["pink","yellow", "red", "rgba(133,122,200)", "#f15025"];
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+  
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+  });
+  
+  function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+  }
